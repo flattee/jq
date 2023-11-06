98aaaa78d648839095ddb753fb152922797965f9
uhtan <flatee27@outlook.jp>
2023-11-06 16:19:05 +0900

    wip

--- sample.py
+++ sample.py
@@ -1,11 +1,6 @@
-from datetime import datetime
-from dateutil import tz
 import jquantsapi
 
 
 cli = jquantsapi.Client()
-df = cli.get_price_range(
-    start_dt=datetime(2022, 7, 25, tzinfo=tz.gettz("Asia/Tokyo")),
-    end_dt=datetime(2022, 7, 26, tzinfo=tz.gettz("Asia/Tokyo")),
-)
-print(df)
+listed_info = cli.get_listed_info()
+listed_info.to_csv("info.csv")
